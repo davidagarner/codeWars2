fd7003d5eb84cbf3465a8ff666ca3a2f430f49e1
Davida Garner <davidac.garner@gmail.com>
2021-05-15 22:37:49 -0400

    codewars

---
+++ drink-about.js
@@ -0,0 +1,20 @@
+function peopleWithAgeDrink(old){
+
+
+    switch(true){
+     case old < 14: 
+      return 'drink toddy';
+    
+      case old < 18: 
+        return 'drink coke';
+    
+      case old < 21: 
+        return 'drink beer';
+    
+      case old >= 21: 
+        return 'drink whisky';
+    
+    }
+    
+    
+    }peopleWithAgeDrink(30)
